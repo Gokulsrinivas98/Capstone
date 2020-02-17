f2b6fb3e517839d187623a6e19646fbc2450f1d7
Gokul <34882860+Gokulsrinivas98@users.noreply.github.com>
2020-02-17 17:02:17 +0530

    Add files via upload

---
+++ BDA/MyCostCapstone2.m
@@ -0,0 +1,9 @@
+function[AF]= MyCostCapstone2(x,u)
+    [M,N] = size(x);
+    AF = zeros(M,length(u));
+%     size(AF)
+    i = (1:N)';
+    for k = 1:M
+        AF(k,:) = abs(2*x(k,:)*cos((2*i-1)*pi*0.5*u))/(2*sum(x(k,:)));
+    end
+end 
---
+++ BDA/mainCapstone2.m
@@ -0,0 +1,63 @@
+clear all 
+close all
+clc
+
+CostFunction=@(x,u) MyCostCapstone2(x,u); % Modify or replace Mycost.m according to your cost funciton
+
+Max_iteration=200; % Maximum number of iterations
+N=200; % Number of particles
+NrElem=100;
+nVar=NrElem/2-2;
+
+% BDA with a v-shaped transfer function
+[Best_pos, Best_score ,Convergence_curve]=BDACapstone2(N, Max_iteration, nVar, CostFunction);
+
+plot(Convergence_curve,'DisplayName','BDA','Color', 'r');
+hold on
+
+title(['Convergence curve']);
+xlabel('Iteration');ylabel('Average Best-so-far');
+legend('BDA');
+box on
+
+
+display(['The best solution obtained by BDA is : ', num2str(Best_pos')]);
+display(['The best optimal value of the objective funciton found by BDA is : ', num2str(Best_score)]);
+
+m = Best_pos';  
+x = linspace(1,Max_iteration,length(m)+2);
+m = [1 m 1]; 
+u = linspace(0,1,1000);
+
+AFopt  = CostFunction(m,u);           Gainopt  = 20*log10(AFopt);
+AForig = CostFunction(ones(1,nVar+2),u);  Gainorig = 20*log10(AForig);
+%% 
+
+figure,
+plot(u,Gainorig,'r','LineWidth',1.25), hold on
+plot(u,Gainopt,'b','LineWidth',1.25)
+
+ylim([-40 0])
+xlabel('u = cos(\phi)')
+ylabel('20log_1_0|FF_n(u)|')
+
+%% 
+
+n=0;
+m = Best_pos';  
+x = linspace(0.1,0.9,length(m)+2);
+m = [1 m 1]; 
+for i = 1:length(m)        
+    if m(i)==1
+        plot(x(i),-10,'ro','MarkerFaceColor','r')
+        text(x(i),-12,num2str(i),'VerticalAlignment','bottom','HorizontalAlignment','center')
+        n=n+1;
+    else
+        plot(x(i),-10,'bo','MarkerFaceColor','g')
+        text(x(i),-12,num2str(i),'VerticalAlignment','bottom','HorizontalAlignment','center')
+    end        
+end
+title({['Optimal Value ',num2str((Best_score),'%3.3f')];['Number of Active Elements = ',num2str(n*2)]})
+legend('Original','Optimized');
+
+
