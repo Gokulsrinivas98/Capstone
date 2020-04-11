886e630bf85457ea1c4bb5b194f06c49f558ebb5
Gokul <34882860+Gokulsrinivas98@users.noreply.github.com>
2020-04-11 20:02:12 +0530

    Update results_new.m

--- review_2_convergence_curve/results_new.m
+++ review_2_convergence_curve/results_new.m
@@ -17,7 +17,7 @@ NrElem2=100;
 nVar2=NrElem2/2-2;
 %%
 %BDA
-[Best_pos, Best_score ,Convergence_curveBDA]=BDACapstone(N1, Max_iteration1, nVar1, NrElem1, CostFunction);
+[Best_pos, Best_score ,Convergence_curveBDA]=BDACapstone(N1, Max_iteration1, nVar1, CostFunction);
 %%
 %BSSA
 [Best_pos, Best_score,Convergence_curveBSSA]= BSSA_capstone(N2, Max_iteration2, nVar2);
@@ -29,15 +29,17 @@ NrElem3=100;
 nVar3=NrElem3/2-2;
 fobj=@(x,u) MyCostCapstone2(x,u);
 [Best_score,Best_pos,Convergence_curveBGWO]=bGWO(SearchAgents_no,Max_iteration3,nVar3,fobj);
-%% 
+% 
 %BGA
-%[minc]=Figure11_13();
+[gen,minc]=Figure11_13();
 %% Figure plotting
-plot(Convergence_curveBDA,'r--','linewidth',1.5);
+plot(Convergence_curveBDA,'y--','linewidth',1.25);
 hold on
-plot(Convergence_curveBSSA,'b*-','linewidth',1.2);
+plot(Convergence_curveBSSA,'b-','linewidth',1.3);
 hold on
-plot(Convergence_curveBGWO,'k','linewidth',1.25);
+plot(Convergence_curveBGWO,'c','linewidth',1.35);
+hold on
+plot(0:gen,minc,'m','LineWidth',1.5),hold on
 
 %plot(Convergence_curveGA,'g','Color',[0 0.4 0],'linewidth',1.5),hold on
 hold off
@@ -45,5 +47,5 @@ grid on
 grid minor
 % title(['Convergence curve']);
 xlabel('Iteration');ylabel('Minimum cost');
-legend('BDA','BSSA','BGWO');
+legend('BDA','BSSA','BGWO','BGA');
 box on
